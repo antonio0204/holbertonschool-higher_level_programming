f836e9b04fe53133558a1e02643ba978439c6b56
ronald0204 <raguirrevil0204@gmail.com>
2020-10-07 04:21:06 +0000

    Task #6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"based on 5-base_geometry.py"
+
+
+class BaseGeometry():
+    """class BaseGeometry
+    """
+    def area(self):
+        """Mwthod area"""
+        raise Exception("area() is not implemented")
