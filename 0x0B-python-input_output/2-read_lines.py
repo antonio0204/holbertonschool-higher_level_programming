92d1846c55d2e9fe7defa594cd43ae2e4acb830e
ronald0204 <raguirrevil0204@gmail.com>
2020-10-08 01:56:44 +0000

    Task #2

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+""" 
+    By: Ronald
+    Read lines
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """
+        print all lines
+        1 line: 'content'
+        2 lien: 'content'
+        3 lien: 'content'
+    """
+    with open(filename, encoding='utf-8') as f:
+        if nb_lines > 0:
+            for i in range(nb_lines):
+                print(f.readline(), end="")
+        else:
+            print(f.read(), end="")
