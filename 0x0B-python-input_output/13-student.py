fe4b9165de800fb0506dd29d59e97521e3277f7a
ronald0204 <raguirrevil0204@gmail.com>
2020-10-08 04:17:52 +0000

    Task #12 #13 #14

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+
+"class function students"
+
+
+class Student:
+    """class studnets
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+            first_name ([type]): [description]
+            last_name ([type]): [description]
+            age ([type]): [description]
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Args:
+            attrs ([type], optional): [description]. Defaults to None.
+        """
+        if attrs is None:
+            return self.__dict__
+
+        mylist = {}
+
+        for att in attrs:
+            if att in self.__dict__:
+                mylist[att] = self.__dict__[att]
+        return mylist
+
+    def reload_from_json(self, json):
+        """
+        Args:
+            json ([type]): [description]
+        """
+        if not json:
+            return
+        self.__dict__ = json
