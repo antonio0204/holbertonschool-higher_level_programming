cdca39a1347cb5923718b59c1e9178b2e0648189
ronald0204 <raguirrevil0204@gmail.com>
2020-10-08 04:06:57 +0000

    Task #11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+"By: Ronald"
+
+
+class Student:
+    """Create s student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+            first_name ([type]): [description]
+            last_name ([type]): [description]
+            age ([type]): [description]
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns:
+            [type]: [description]
+        """
+        return self.__dict__
