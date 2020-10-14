f06f1d387eb54f7f1a188c1f380ed987a4ae0b3b
ronald0204 <raguirrevil0204@gmail.com>
2020-10-14 00:01:26 +0000

    Test

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python3
+"""Test program base"""
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+from io import StringIO
+import io
+import sys
+import pep8
+
+
+class TestRectangle(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        Base._Base__nb_objects = 0
+
+    def test_inheritance(self):
+        r1 = Rectangle(1, 1)
+        self.assertIsInstance(r1, Base)
+
+    def test_rect_id(self):
+        r2 = Rectangle(4, 3, 4, 1, 35)
+        self.assertEqual(r2.id, 35)
+
+    def test_rectangle(self):
+        r4 = Rectangle(3, 5, 6, 8, 10)
+        self.assertEqual(r4.width, 3)
+        self.assertEqual(r4.height, 5)
+        self.assertEqual(r4.x, 6)
+        self.assertEqual(r4.y, 8)
+        self.assertEqual(r4.id, 10)
+
+    def test_rectangle_valid_type_error(self):
+        self.assertRaises(TypeError, Rectangle, "Deiwin", 1)
+        self.assertRaises(TypeError, Rectangle, 1, "Deiwin")
+        self.assertRaises(TypeError, Rectangle, [1], 2)
+        self.assertRaises(TypeError, Rectangle, 1.1, 1)
+        self.assertRaises(TypeError, Rectangle, (1, ), 1)
+        self.assertRaises(TypeError, Rectangle, 1.1, 1, 1, 1)
+
+    def test_rectangle_valid_value_error(self):
+        self.assertRaises(ValueError, Rectangle, -1, 1)
+        self.assertRaises(ValueError, Rectangle, 1, -100)
+        self.assertRaises(ValueError, Rectangle, 0, 1)
+        self.assertRaises(ValueError, Rectangle, 1, 0)
+        self.assertRaises(ValueError, Rectangle, 1, 1, -2, 1)
+
+    def test_area(self):
+        r4 = Rectangle(2, 2)
+        self.assertEqual(r4.area(), 4)
+
+    def test_display(self):
+        r5 = Rectangle(3, 3)
+        old_stdout = sys.stdout
+        sys.stdout = mystdout = StringIO()
+
+        r5.display()
+        self.assertEqual(sys.stdout.getvalue(), "###\n###\n###\n")
+
+    def test_str(self):
+        r6 = Rectangle(1, 2)
+        list_string = r6.__str__()
+        self.assertEqual(list_string, "[Rectangle] (1) 0/0 - 1/2")
+
+    def test_display_1(self):
+        r7 = Rectangle(2, 3, 2, 2)
+        old_stdout = sys.stdout
+        sys.stdout = mystdout = StringIO()
+        r7.display()
+        self.assertEqual(sys.stdout.getvalue(), "\n\n  ##\n  ##\n  ##\n")
+
+    def test_update_args(self):
+        r11 = Rectangle(9, 9, 9, 9)
+        r11.update(89, 2, 3, 4, 5)
+        self.assertEqual(r11.width, 2)
+        self.assertEqual(r11.height, 3)
+        self.assertEqual(r11.x, 4)
+        self.assertEqual(r11.y, 5)
+        self.assertEqual(r11.id, 89)
+        self.assertRaises(TypeError, r11.update, 45, "Deiwin")
+        self.assertRaises(TypeError, r11.update, 1, 1, "Monsalve")
+        self.assertRaises(TypeError, r11.update, 1, 1, 1, "Altamar")
+        self.assertRaises(ValueError, r11.update, 89, -1, 1)
+        self.assertRaises(ValueError, r11.update, 89, 1, -1)
+        self.assertRaises(ValueError, r11.update, 89, 1, 1, -1)
+
+    def test_update_kwargs(self):
+        r1 = Rectangle(10, 10, 10, 10)
+
+        r1.update(height=1)
+        self.assertEqual(r1.height, 1)
+
+        r1.update(width=1, x=2)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.x, 2)
+
+        r1.update(y=1, width=2, x=3, id=89)
+        self.assertEqual(r1.y, 1)
+        self.assertEqual(r1.width, 2)
+        self.assertEqual(r1.x, 3)
+        self.assertEqual(r1.id, 89)
+        self.assertRaises(TypeError, r1.update, x='Holberton')
+        self.assertRaises(TypeError, r1.update, y=3.5)
+        self.assertRaises(TypeError, r1.update, height=(1,))
+        self.assertRaises(TypeError, r1.update, width=[2])
+        self.assertRaises(ValueError, r1.update, height=-1)
+        self.assertRaises(ValueError, r1.update, width=-1)
+        self.assertRaises(ValueError, r1.update, x=-1)
+        self.assertRaises(ValueError, r1.update, y=-1)
+
+    def test_to_dict(self):
+        r1 = Rectangle(10, 2, 1, 9)
+        r1_dictionary = r1.to_dictionary()
+        dic_comparation = {'x': 1, 'y': 9, 'id': 1, 'height': 2, 'width': 10}
+        self.assertEqual(r1_dictionary, dic_comparation)
+
+        r2 = Rectangle(1, 1)
+        r2.update(**r1_dictionary)
+        self.assertEqual(r1 == r2, False)
+
+    def test_style_base(self):
+        """test pep8
+        """
+        style = pep8.StyleGuide()
+        m = style.check_files(["models/rectangle.py"])
+        self.assertEqual(m.total_errors, 0, "fix pep8")
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python3
+"""Test program base"""
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+from io import StringIO
+import io
+import sys
+import pep8
+
+
+class TestSquare(unittest.TestCase):
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_square_inherencia(self):
+        s1 = Square(1)
+        self.assertIsInstance(s1, Rectangle)
+
+    def test_square_id(self):
+        s2 = Square(3)
+        self.assertEqual(s2.id, 1)
+
+    def test_square_width_and_heigth(self):
+        s2 = Square(3)
+        self.assertEqual(s2.width, 3)
+        self.assertEqual(s2.height, 3)
+
+    def test_square_x_and_y(self):
+        s3 = Square(5, 1, 1)
+        self.assertEqual(s3.x, 1)
+        self.assertEqual(s3.y, 1)
+
+    def validation_type_or_value(self):
+        self.assertRaises(TypeError, Square, 1, "Holberton")
+        self.assertRaises(TypeError, Square, (1, ), 1)
+        self.assertRaises(TypeError, Square, [1], 1)
+        self.assertRaises(ValueError, Square, 1, -1)
+        self.assertRaises(ValueError, Square, -1, 1)
+        self.assertRaises(ValueError, Square, 3.5, 5)
+
+    def test_area(self):
+        s1 = Square(3)
+        self.assertEqual(s1.area(), 9)
+
+    def test_square_display(self):
+        s1 = Square(2)
+        old_stdout = sys.stdout
+        sys.stdout = mystdout = StringIO()
+
+        string_test = s1.__str__()
+        s1.display()
+        self.assertEqual(string_test, "[Square] (1) 0/0 - 2")
+        self.assertEqual(s1.area(), 4)
+        self.assertEqual(sys.stdout.getvalue(), "##\n##\n")
+
+    def test_display_espacies(self):
+        old_stdout = sys.stdout
+        sys.stdout = mystdout = StringIO()
+        s2 = Square(2, 2, 2)
+        s2.display()
+        self.assertEqual(mystdout.getvalue(), "\n\n  ##\n  ##\n")
+
+    def test_update_args(self):
+        s11 = Square(9, 9, 9)
+        s11.update(89, 2, 3, 4)
+        self.assertEqual(s11.size, 2)
+        self.assertEqual(s11.x, 3)
+        self.assertEqual(s11.y, 4)
+        self.assertEqual(s11.id, 89)
+        self.assertRaises(TypeError, s11.update, 45, "Deiwin")
+        self.assertRaises(TypeError, s11.update, 1, 1, "Monsalve")
+        self.assertRaises(TypeError, s11.update, 1, 1, 1, "Altamar")
+        self.assertRaises(ValueError, s11.update, 89, -1, 1)
+        self.assertRaises(ValueError, s11.update, 89, 1, -1)
+        self.assertRaises(ValueError, s11.update, 89, 1, 1, -1)
+
+    def test_update_kwargs(self):
+        r1 = Square(10, 10, 10)
+
+        r1.update(size=1)
+        self.assertEqual(r1.size, 1)
+
+        r1.update(size=1, x=2)
+        self.assertEqual(r1.size, 1)
+        self.assertEqual(r1.x, 2)
+
+        r1.update(y=1, size=2, x=3, id=89)
+        self.assertEqual(r1.y, 1)
+        self.assertEqual(r1.size, 2)
+        self.assertEqual(r1.x, 3)
+        self.assertEqual(r1.id, 89)
+        self.assertRaises(TypeError, r1.update, x='Holberton')
+        self.assertRaises(TypeError, r1.update, y=3.5)
+        self.assertRaises(TypeError, r1.update, size=(1,))
+        self.assertRaises(ValueError, r1.update, size=-1)
+        self.assertRaises(ValueError, r1.update, x=-1)
+        self.assertRaises(ValueError, r1.update, y=-1)
+
+    def test_to_dict(self):
+        s1 = Square(10, 1, 9)
+        s1_dictionary = s1.to_dictionary()
+        dic_comparation = {'x': 1, 'y': 9, 'id': 1, 'size': 10}
+        self.assertEqual(s1_dictionary, dic_comparation)
+
+        s2 = Rectangle(1, 1)
+        s2.update(**s1_dictionary)
+        self.assertEqual(s1 == s2, False)
+
+    def test_style_base(self):
+        """test pep8
+        """
+        style = pep8.StyleGuide()
+        m = style.check_files(["models/square.py"])
+        self.assertEqual(m.total_errors, 0, "fix pep8")
+
+
+if __name__ == '__main__':
+    unittest.main()
