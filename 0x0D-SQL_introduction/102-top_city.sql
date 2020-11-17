1b24113f5ef583aaf04787e8259ad0116e2f6aee
ronald0204 <raguirrevil0204@gmail.com>
2020-11-17 02:34:04 +0000

    All file

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,4 @@
+-- Import in hbtn_0c_0 database this table
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- CREATE TABLE MYSQL
+
+CREATE TABLE IF NOT EXISTS first_table (id INT,
+name VARCHAR(256));
