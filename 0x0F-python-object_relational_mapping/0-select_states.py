bd459eea11c138e4ae8e0586dc5b3c0230cd166e
ronald0204 <ronaldaguirre0204@gmail.com>
2020-12-13 17:00:19 -0500

    Add file conection db.2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     import MySQLdb
     from sys import argv
 
-    db = MySQLdb.connect('192.168.1.8', argv[1], argv[2], argv[3], port=3306)
+    db = MySQLdb.connect(host='localhost', argv[1], argv[2], argv[3], port=3306)
     cur = db.cursor()
 
     cur.execute('SELECT * FROM states ORDER BY id ASC')
