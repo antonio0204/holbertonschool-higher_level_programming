b4f31a113e254890f187624b3200158097f83fd0
ronald0204 <ronaldaguirre0204@gmail.com>
2020-12-13 17:46:52 -0500

    Add file conection db.8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-'''
-    MySQLdb
-'''
-if __name__ == "__main__":
+"""MySQLdb"""
+if __name__ == '__main__':
 
-    import sys
     import MySQLdb
+    import sys
 
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("""SELECT * FROM states
-                ORDER BY states.id ASC""")
-    for row in c.fetchall():
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    line = cur.fetchall()
+    for row in line:
         print(row)
-    c.close()
-    db.close()
