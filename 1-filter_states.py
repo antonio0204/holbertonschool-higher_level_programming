31a53be8e44ddbecdf80df688431d32661573baa
ronald0204 <ronaldaguirre0204@gmail.com>
2020-12-14 01:35:44 +0000

    Task #1

---
+++ 1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""[lists all states with a name starting with N]
+"""
+import MySQLdb
+import sys
+
+
+def filterStates():
+    """name starting with N (upper N)
+    """
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id ASC')
+    dataRow = cur.fetchall()
+    for row in dataRow:
+        if row[1][0] == 'N':
+            print(row)
+
+    db.close()
+
+
+if __name__ == '__main__':
+    filterStates()
