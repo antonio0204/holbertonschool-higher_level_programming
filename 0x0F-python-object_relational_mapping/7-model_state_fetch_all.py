908590a79c3c25064210419589eba076f9d5e7eb
ronald0204 <ronaldaguirre0204@gmail.com>
2020-12-14 02:39:41 +0000

    Task #2

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""Write a script that lists all 
-"""
-import sys
+"""Start link class to table"""
+
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
-def allStatesSQLAlchemy():
-    """lists all State objects from the database
-    """
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]),
-        pool_pre_ping=True)
+if __name__ == "__main__":
+
+    connect = 'mysql+mysqldb://{}:{}@localhost:3306/{}'. \
+        format(argv[1], argv[2], argv[3])
+
+    engine = create_engine(connect, pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
+    query = session.query(State).order_by(State.id).all()
 
-
-if __name__ == '__main__':
-    allStatesSQLAlchemy()
+    for row in query:
+        print("{}: {}".format(row.id, row.name))
