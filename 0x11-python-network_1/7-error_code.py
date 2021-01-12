bcf34ea4a788356f14d560859969618e6a0be336
ronald0204 <ronaldaguirre0204@gmail.com>
2021-01-11 20:01:13 -0500

    hot fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""sends a request to the URL"""
-import requests
+"""sends a request to the URL and displays the body"""
 import sys
+import requests as rq
 
 
-def errorCode(url):
-    """sends a request to the URL"""
-    r = requests.get(url)
-    if (r.status_code == 200):
-        print(r.text)
-    else:
-        print("Error code: {}".format(r.status_code))
-
-
-if __name__ == '__main__':
-    errorCode(sys.argv[1])
+if __name__ == "__main__":
+        r = rq.get(sys.argv[1])
+        if r.status_code >= 400:
+            print('Error code: {}'.format(r.status_code))
+        else:
+            print(r.text)
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""script that takes in a letter"""
-import requests
+"""sends a POST request to http://0.0.0.0:5000/search_user"""
 import sys
+import requests as rq
 
 
-def search_api(url, q):
-    """script that takes in a letter"""
-    data = {'q': q}
-    r = requests.post(url, data=data)
-
-    try:
-        n_dict = r.json()
-        if n_dict:
-            print("[{}] {}".format(n_dict['id'], n_dict['name']))
-        else:
-            print("No result")
-    except Exception:
-        print("Not a valid JSON")
-
-
-if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        q = sys.argv[1]
-    else:
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
         q = ""
-    url = "http://0.0.0.0:5000/search_user"
-    search_api(url, q)
+    else:
+        q = sys.argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+    r = rq.post(url, data={'q': q})
+    try:
+        data = r.json()
+        if data:
+            print('[{}] {}'.format(data['id'], data['name']))
+
+        else:
+            print('No result')
+    except:
+        print('Not a valid JSON')
