5cdf83d318d055564f2a437f4d9a76baa2081b6c
ronald0204 <ronaldaguirre0204@gmail.com>
2021-02-02 17:25:12 -0500

    Task #3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* JavaScript
+   Author: Ronald Aguirre
+   Cuorse: JavaScript Warm up
+*/
+
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
