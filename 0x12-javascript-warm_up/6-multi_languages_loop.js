c30a739150f73ed0d8f4c0829427b15b298330f5
ronald0204 <ronaldaguirre0204@gmail.com>
2021-02-02 17:36:52 -0500

    Task #6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* JavaScript
+   Author: Ronald Aguirre
+   Cuorse: JavaScript Warm up
+*/
+
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let index = 0; index < array.length; index++) {
+  console.log(array[index]);
+}
