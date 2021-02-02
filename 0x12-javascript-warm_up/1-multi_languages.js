36465475d4951d27a5448305dc1517692ab01c3f
ronald0204 <ronaldaguirre0204@gmail.com>
2021-02-02 14:16:47 -0500

    Add my firts constant with an array

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* JavaScript
+   Author: Ronald Aguirre
+   Cuorse: JavaScript Warm up
+*/
+
+// Declare a constant with an array
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+// We print through an array function with the function forEach
+lines.forEach(element => console.log(element));
