746d506e542e2c3ffe37411e232a39f0f6109c6e
ronald0204 <ronaldaguirre0204@gmail.com>
2021-02-02 17:30:50 -0500

    Task #4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/* JavaScript
+   Author: Ronald Aguirre
+   Cuorse: JavaScript Warm up
+*/
+
+if (process.argv.length <= 2) {
+  console.log('undefined is undefined');
+} else if (process.argv[3] === undefined) {
+  console.log(process.argv[2] + ' is ' + 'undefined');
+} else {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+}
