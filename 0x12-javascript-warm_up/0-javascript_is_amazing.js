abfddca9d5e6220d1b1fa8ecc91af4848b919bf3
ronald0204 <ronaldaguirre0204@gmail.com>
2021-02-02 12:36:56 -0500

    Requirements and Firts JS

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* JavaScript
+   Author: Ronald Aguirre
+   Cuorse: JavaScript Warm up
+*/
+
+// Dclaro la variable con Let
+const myVar = ('Javascript is amazing');
+
+// imrpimimos por consola
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,6 @@
+# 0x12. Javascript - Warm up
+
+## Author
+---
+- **GitHub**[@ronald0204](https://github.com/ronald0204)
+- **Twiitter:** [@ronald45251997](https://twitter.com/ronald45251997)
