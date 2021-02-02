21fe8012ce3427f89acc84d5fbd619bf9dfe1deb
ronald0204 <ronaldaguirre0204@gmail.com>
2021-02-02 17:42:11 -0500

    Task #7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/* JavaScript
+   Author: Ronald Aguirre
+   Cuorse: JavaScript Warm up
+*/
+
+const len = process.argv;
+if (process.argv[2] === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let index = 0; index < len[2]; index++) {
+    console.log('C is fun');
+  }
+}
